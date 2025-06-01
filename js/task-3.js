3435999e0a99cd51cbe49b8e62312bb53e44d864
yangelia <yana.sychova@gmail.com>
2025-06-02 00:06:55 +0300

    upd

--- js/task-3.js
+++ js/task-3.js
@@ -7,3 +7,8 @@ function filterArray(numbers, value) {
   }
   return arr;
 }
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
